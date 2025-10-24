55e712ef160ee55d66b161bc7b6619474265f90e
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-24 12:28:00 -0700

    Add 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  if (
+    isNaN(startNum) ||
+    isNaN(endNum) ||
+    startNum < 0 ||
+    endNum < 0 ||
+    startNum % 1 != 0 ||
+    endNum % 1 != 0 ||
+    typeof startNum === "string" ||
+    typeof endNum === "string"
+  ) {
+    return "ERROR";
+  }
+  if (startNum > endNum) {
+    [startNum, endNum ] = [endNum, startNum];
+  }
+  let total = 0;
+  for (let i = startNum; i <= endNum; i++) {
+    total += i;
+  }
+  return total;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
