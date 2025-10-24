44e73dbc328540bb26971f13f77e60d2c3a55437
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-24 13:14:08 -0700

    Add 11-tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+// round both to one decimal
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((5 * (fahrenheit - 32) / 9) * 10) / 10;
 };
-
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100))
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
