85f44680a611601dbcf4c5234e72d42014677a8c
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-24 11:36:40 -0700

    06-repeatString solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0){
+        return 'ERROR'
+    }
+    let repeatedString = '';
+    for (let i = 0; i < times; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
