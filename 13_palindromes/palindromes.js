97c13900d796d20fc21a8f7cc2d605a67914fa23
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-27 10:54:55 -0700

    Add 13_palindromes solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g,"");
+    let reversedStr = str.split('').reverse().join('')
+    return str === reversedStr;
 };
-
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
