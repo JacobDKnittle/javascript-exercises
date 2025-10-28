244257c1604778bec478448ffd22040af5379bb9
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-27 22:14:56 -0700

    Add 14_fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS'
+    let nums = [0,1];
+    for (let i = 0; i < +num; i++) {
+        nums.push(nums[i] + nums[i + 1]);
+    }
+    return nums[+num];
 };
 
+fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 // Do not edit below this line
 module.exports = fibonacci;
