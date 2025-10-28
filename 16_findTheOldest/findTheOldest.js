d7a224e0dbd0ca5c79f1050fd2ebf71913fa5766
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-28 10:24:38 -0700

    Add 16_findTheOldest solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let ages = people.map((person) => (person.yearOfDeath || 2025) - person.yearOfBirth);
+    let oldestAgeIndex = 0;
+    let oldestAge = 0;
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > oldestAge){
+            oldestAge = ages[i];
+            oldestAgeIndex = i;
+        }
+    }
 
+    return people[oldestAgeIndex];
 };
 
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+    console.log(findTheOldest(people).name);
 // Do not edit below this line
 module.exports = findTheOldest;
